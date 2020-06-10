6b4126a04e8bcfb2ffe5b1c88c66767e5b7499dc
uartman <uartman@mail.ru>
2020-06-10 19:51:00 +0300

    init repo

---
+++ launch.py
@@ -0,0 +1,14 @@
+from sanic import Sanic
+from sanic.response import json
+from sanic.request import Request
+from config import WEB_SERVER_PORT
+app = Sanic(name="BTTK")
+
+
+@app.route("/")
+async def test(request: Request):
+    return json({"hello": "world"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=WEB_SERVER_PORT)
