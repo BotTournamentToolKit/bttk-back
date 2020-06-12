d515f6e8fa226b3f0cda8719fe9c1cabf86c029a
uartman <uartman@mail.ru>
2020-06-12 11:40:32 +0300

    add file_name and parent_path. to executor

--- executor/__init__.py
+++ executor/__init__.py
@@ -1,10 +1,18 @@
+from pathlib import Path
+
+
 class Executor(object):
-    file_path: str
+    file_name: str
     timeout: float
+    parent_path: str
+    file_name: str
 
     def __init__(self, path: str, timeout: float = 5.0):
-        self.file_path = path
+        self.file_name = path
         self.timeout = timeout
+        pth = Path(self.file_name)
+        self.parent_path = str(pth.parent)
+        self.file_name = str(pth.name)
 
     async def turn(self, field: str) -> str:
         raise NotImplementedError()
