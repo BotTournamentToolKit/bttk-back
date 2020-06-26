bfa3a4a3c27adb1237215026e9929f0bd4ff2c9e
uartman <uartman@mail.ru>
2020-06-26 13:51:23 +0300

    add loop correction to launch file too

--- launch.py
+++ launch.py
@@ -1,3 +1,6 @@
+import asyncio
+import platform
+
 from sanic import Sanic
 from sanic.response import json
 from sanic.request import Request
@@ -11,5 +14,14 @@ async def test(request: Request):
     return json({"hello": "world"})
 
 
+def correct_loop():
+    if platform.system() == "Windows":
+        loop = asyncio.ProactorEventLoop()
+    else:
+        loop = asyncio.get_event_loop()
+    return loop
+
+
 if __name__ == "__main__":
+    asyncio.set_event_loop(correct_loop())
     app.run(host="0.0.0.0", port=WEB_SERVER_PORT)
